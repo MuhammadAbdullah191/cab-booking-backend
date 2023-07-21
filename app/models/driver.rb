d432dac1fde19ac395ab6d7577d4df54a7a62b7d
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2023-07-21 14:05:52 +0500

    Application: Added Crud for car controller -> Added car serializer

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,4 +2,6 @@ class Driver < User
   validates :cnic, presence: true, length: { maximum: 20 }
   validates :license_number, presence: true, length: { maximum: 20 }
   validates :license_expiry, presence: true
+
+  has_one :car
 end
