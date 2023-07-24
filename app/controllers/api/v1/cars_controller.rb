aafff5eecff145d54bd12ad126625ba75399b5a4
MuhammadAbdullah191 <abdullah.badar@devsinc.com>
2023-07-24 12:11:26 +0500

    CarsController: Fixed Spacing issue

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,35 +1,35 @@
 class Api::V1::CarsController < ApplicationController
-	before_action :set_car, only: %i(update destroy)
+  before_action :set_car, only: %i(update destroy)
 
-	def index
-		@cars = Car.all
-		render json: { message: 'Cars loaded successfully', cars: @cars }, status: :ok
-	end
+  def index
+    @cars = Car.all
+    render json: { message: 'Cars loaded successfully', cars: @cars }, status: :ok
+  end
 
-	def create
-		@car = Car.new(car_params)
-		if @car.save
-			render json: { message: 'Car created successfully', car: @car }, status: :ok
-		else
-			render json: { message: @car.errors.full_messages.to_sentence }, status: :unprocessable_entity
-		end
-	end
-	
-	def update
-		if @car.update(car_params)
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      render json: { message: 'Car created successfully', car: @car }, status: :ok
+    else
+      render json: { message: @car.errors.full_messages.to_sentence }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @car.update(car_params)
       render json: { user: CarSerializer.new(@car), message: 'User updated successfully' }, status: :ok
     else
       render json: { message: @car.errors.full_messages.to_sentence }, status: :unprocessable_entity
     end
-	end
+  end
 
-	private
+  private
 
-	def set_car
-		@car = set_instance(car_params[:id], Car, '')
-	end
+  def set_car
+    @car = set_instance(car_params[:id], Car, '')
+  end
 
-	def car_params
-		params.permit(:id, :reg_number, :make, :model, :seats, :user_id)
-	end
+  def car_params
+    params.permit(:id, :reg_number, :make, :model, :seats, :user_id)
+  end
 end
